c6a780248eaae7a9790ec2627fa172ca79508a81
Juraj Kusnier <jurajkusnier@gmail.com>
2021-12-01 22:05:15 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun List<Int>.getIncreasesSum() = zipWithNext().count { (a, b) -> a < b }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<Int>): Int = input.getIncreasesSum()
+
+    fun part2(input: List<Int>): Int = input.windowed(3).map { it.sum() }.getIncreasesSum()
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01").map { it.toInt() }
+    println(part1(input)) // 1292
+    println(part2(input)) // 1262
 }
